e707adefca6c0b3fd42a0c5d138c890aea96d370
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-24 21:19:42 +0700

    ket thuc

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ app.get('/show', (req, res) => res.render('home', { name: 'KHoa PHAm' }));
 app.get('/admin', (req, res) => {
     res.render('admin', { isAdmin: true });
 });
+
+class Employee {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const arrEmp = [
+    new Employee('Khoa', 30),
+    new Employee('Phat', 29),
+    new Employee('Tien', 25)
+];
+
+app.get('/loop', (req, res) => {
+    res.render('nhanvien', { mang: arrEmp });
+});
